c8b7c85d841515654ef812eef6d37d47024421d8
Jonathan <warkentin.jon@gmail.com>
2019-12-15 15:01:58 -0600

    fix file path stuff, add saving options

--- clockin.py
+++ clockin.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Font
 from os import getenv, path
 
@@ -7,10 +7,13 @@ from os import getenv, path
 def start_work():
     # set file name from environment variable
     file_name = getenv('TIMESHEET')
-    if file_name == "":
-        file_name = None
-    elif file_name.split('.')[1] != 'xlsx':
-        file_name = file_name.split('.')[0] + '.xlsx'
+    if file_name is not None:
+        if file_name == "":
+            file_name = "timesheet.xlsx"
+        elif file_name.split('.')[1] != 'xlsx':
+            file_name = file_name.split('.')[0] + '.xlsx'
+    else:
+        file_name = "timesheet.xlsx"
 
     # start work timing
     print('Starting work...')
@@ -29,7 +32,23 @@ def start_work():
     # check if inputted file exists
     # if yes, write to that file - assume correct format
     if path.exists(file_name):
+        workbook = load_workbook(file_name)
+        sheet = workbook.active
+        row_number = sheet.max_row
+        last_date = sheet['A' + str(row_number)]
+        todays_date = datetime.now().strftime("%x")
+        # if last entry from same day, append new info to that
+        if last_date.value == todays_date:
+            sheet['B' + str(row_number)] = str(float(sheet['B' + str(row_number)].value) + hours)
+            sheet['C' + str(row_number)] = sheet['C' + str(row_number)].value + '; ' + task
+        # else add new row with current date
+        else:
+            sheet["A" + str(row_number + 1)] = datetime.now().strftime("%x")
+            sheet["B" + str(row_number + 1)] = str(hours)
+            sheet["C" + str(row_number + 1)] = task
+        workbook.save(filename=file_name)
         print("Saved work session to existing file: " + file_name)
+
     # if no, write to new file with default name
     else:
         workbook = Workbook()
@@ -44,7 +63,6 @@ def start_work():
         sheet["A2"] = datetime.now().strftime("%x")
         sheet["B2"] = str(hours)
         sheet["C2"] = task
-        file_name = "timesheet.xlsx"
         workbook.save(filename=file_name)
         print("Saved work session to new file: " + file_name)
 
